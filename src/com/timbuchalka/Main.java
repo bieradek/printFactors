372f06a98229688a13a584f2d0971113e6e3fdd2
bieradek <biedrzycki.radek@gmail.com>
2018-07-26 12:30:15 +0200

    Uploading the final String solution along with other people's code

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -6,17 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(printFactors(10));
+        System.out.println(printFactors(25));
+        System.out.println(printFactors(16));
     }
 
-    public static StringBuilder printFactors(int number) {
+    public static String printFactors(int number) {
         if (number < 1) {
-            StringBuilder invalidValue = new StringBuilder("Invalid Value");
-            return invalidValue;
+            return "Invalid Value";
         }
-        StringBuilder factorsConcatenated = new StringBuilder("1");
+        String factorsConcatenated = "1";
         for (int factor = 2; factor <= number; factor++) {
             if (number % factor == 0) {
-                factorsConcatenated = factorsConcatenated.append(" " + factor);
+                factorsConcatenated = factorsConcatenated + " " + factor;
             }
         }
         return factorsConcatenated;
@@ -44,6 +45,41 @@ usually it is preferable to replace it with explicit calls to StringBuilder.appe
     }
 
  */
+
+/*
+*******************OTHER PEOPLE CODES*****************
+     public static void printFactors(int number){
+        if(number<1){
+            System.out.println("Invalid Value");
+        }
+        else{
+            int i=1;
+            while(i<=number){
+                if(number%i==0){
+                    System.out.println(i);
+                }
+                i++;
+            }
+        }
+    }
+ */
+
+
+/*
+    public static String printFactors(int number) {
+        if (number < 1) {
+            return "Invalid Value";
+        }
+        String factorsConcatenated = "1";
+        for (int factor = 2; factor <= number; factor++) {
+            if (number % factor == 0) {
+                factorsConcatenated = factorsConcatenated + "" + factor;
+            }
+        }
+        return factorsConcatenated;
+    }
+ */
+
 }
 
 
