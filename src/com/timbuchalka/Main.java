b8fe03816f15616a3ebaa170bdd285b76ad1cf7a
bieradek <biedrzycki.radek@gmail.com>
2018-07-26 12:58:35 +0200

    Final solution using a void method and while loop

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -5,23 +5,39 @@ import java.lang.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(printFactors(10));
-        System.out.println(printFactors(25));
-        System.out.println(printFactors(16));
+//        System.out.println(printFactors(10));
+//        System.out.println(printFactors(25));
+//        System.out.println(printFactors(16));
+        printFactors(10);
+        printFactors(25);
+        printFactors(16);
     }
 
-    public static String printFactors(int number) {
+    private static void printFactors(int number) {
         if (number < 1) {
-            return "Invalid Value";
+            System.out.println("Invalid Value");
         }
-        String factorsConcatenated = "1";
-        for (int factor = 2; factor <= number; factor++) {
+        int factor = 1;
+        while (factor <= number) {
             if (number % factor == 0) {
-                factorsConcatenated = factorsConcatenated + " " + factor;
+                System.out.println(factor);
             }
+            factor = factor + 1;
         }
-        return factorsConcatenated;
     }
+
+//    public static String printFactors(int number) {
+//        if (number < 1) {
+//            return "Invalid Value";
+//        }
+//        String factorsConcatenated = "1";
+//        for (int factor = 2; factor <= number; factor++) {
+//            if (number % factor == 0) {
+//                factorsConcatenated = factorsConcatenated + " " + factor;
+//            }
+//        }
+//        return factorsConcatenated;
+//    }
 /*
 String concatenation '+' in loop less... (Ctrl+F1)
 Reports String concatenation in loops. As every String concatenation copies the whole String,
